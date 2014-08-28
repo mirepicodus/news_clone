7af6dbe84d254f5046523b24622d7084a47cbbbc
mark rubenstein <student@epicodus.com>
2014-08-28 15:52:30 -0700

    add basic auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-
+  has_secure_password
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  # validates :email, uniqueness: true
+  # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
