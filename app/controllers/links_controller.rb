f4062c2655ac33c45660bd6a29004b38799baaba
mark rubenstein <student@epicodus.com>
2014-08-28 17:18:08 -0700

    wip

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,27 @@
 class LinksController < ApplicationController
 
+  before_filter :authorize, only: [:new, :edit, :update]
+
   def index
     @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path, notice: "Thanks for submitting!"
+    else
+      render "new"
+    end
+  end
+
+private
+  def link_params
+    params.require(:link).permit(:name, :url, :current_user)
   end
 
 end
